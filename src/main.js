670337086d0bf27e9199217bf2d5547f33372e3e
Brian Sturgis <sturujisu@gmail.com>
2021-02-10 15:15:02 -0800

    wil complete rover pic of the day

--- src/main.js
+++ src/main.js
@@ -3,22 +3,58 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import MartianWeather from "./martianWeather.js";
+import { MartianWeather, MartianPic, MartianRover } from "./martianWeather.js";
 
-function getElements(response) {
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showWeather').text("");
+  $('.showPic').text("");
+  $('.showRoverPic').text("");
+}
+
+function getWeather(response) {
   if (response[785]) {
-    $('.showWeather').html(`Atmospheric Pressure on Mars: ${response[785].PRE.av}`);
-    console.log(response[785].PRE.av);
+    $(".showWeather").html(
+      `Atmospheric Pressure on Mars: ${response[785].PRE.av}`
+    );
   } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
+    $(".showErrors").text(`There was an error: ${response.message}`);
   }
 }
- 
-$(document).ready(function() {
-  $('#marsWeather').click(function() {
-    MartianWeather.getWeather()
-      .then(function(response) {
-        getElements(response);
-      });
+function getPic(responsePic) {
+  if (responsePic) {
+    $(".showPic").html(`Astronomy Pic of The Day: <img src=${responsePic.url}>`);
+  } else {
+    $(".showErrors").text(`There was an error: ${responsePic.message}`);
+  }
+}
+function getRoverPic(responseRoverPic) {
+  if (responseRoverPic) {
+    $(".showRoverPic").html(`Mars Rover Pic of The Day: <img src=${responseRoverPic.photos[0].img_src}>`);
+  } else {
+    $(".showErrors").text(`There was an error: ${responseRoverPic.message}`);
+  }
+}
+
+$(document).ready(function () {
+  $("#marsWeather").click(function () {
+    clearFields()
+    MartianWeather.getWeather().then(function (response) {
+      getWeather(response);
+    });
+  });
+  $("#marsPic").click(function () {
+    clearFields()
+    MartianPic.getPic().then(function (responsePic) {
+      getPic(responsePic);
+    });
+  });
+  $("#marsRoverPic").click(function () {
+    let date = $('#date').val();
+    clearFields()
+    MartianRover.getRoverPic(date).then(function (responseRoverPic) {
+      getRoverPic(responseRoverPic);
+    });
   });
 });
--- src/martianWeather.js
+++ src/martianWeather.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-export default class MartianWeather {
+export class MartianWeather {
   static getWeather() {
     return fetch(`https://api.nasa.gov/insight_weather/?api_key=${process.env.API_KEY}&feedtype=json&ver=1.0`)
       .then(function(response) {
@@ -13,3 +13,34 @@ export default class MartianWeather {
       });
   }
 }
+
+export class MartianPic {
+  static getPic() {
+    return fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+      .then(function(responsePic) {
+        if (!responsePic.ok) {
+          throw Error(responsePic.statusText);
+        }
+        return responsePic.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
+
+export class MartianRover {
+  static getRoverPic(date) {
+    return fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${process.env.API_KEY}`)
+      .then(function(responseRoverPic) {
+        if (!responseRoverPic.ok) {
+          throw Error(responseRoverPic.statusText);
+        }
+        return responseRoverPic.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
+
